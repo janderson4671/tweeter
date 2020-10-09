e896d2b0695908280110671e60956192a5ed2db8
Jason Douglas Anderson <jande223@ta-16.cs.byu.edu>
2020-10-09 12:45:55 -0600

    Stopping point

--- app/src/main/java/edu/byu/cs/tweeter/view/main/mainFragments/NewStatusFragment.java
+++ app/src/main/java/edu/byu/cs/tweeter/view/main/mainFragments/NewStatusFragment.java
@@ -1,4 +1,63 @@
 package edu.byu.cs.tweeter.view.main.mainFragments;
 
-public class NewStatusFragment  {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import edu.byu.cs.tweeter.R;
+
+public class NewStatusFragment extends DialogFragment {
+
+    //widgets
+    Button cancelButton;
+    Button postButton;
+
+    EditText statusMessageText;
+
+    //Empty constructor for DialogFragment
+    public NewStatusFragment() {}
+
+    public static NewStatusFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        NewStatusFragment fragment = new NewStatusFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.new_status_fragment, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        wireWidgets(view);
+        setListeners();
+
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
+
+    private void setListeners() {
+        //TODO:: Set Listeners here!
+    }
+
+    private void wireWidgets(View v) {
+        cancelButton = v.findViewById(R.id.cancelButton);
+        postButton = v.findViewById(R.id.postButton);
+
+        statusMessageText = v.findViewById(R.id.statusMessageTextBox);
+    }
+
 }
